d7cfa5f39c23cd7e1979f5fe501092f67ba3f48c
Nolan Morache <nolan241@gmail.com>
2017-01-07 18:22:00 +0000

    finished dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,9 @@ class PagesController < ApplicationController
   end
   
   def dashboard
-    @books = current_user.books
+  	@books = current_user.books
+    @purchased = Sale.where(buyer_email: current_user.email)
+    @sales = Sale.where(seller_email: current_user.email)
   end
   
 end
